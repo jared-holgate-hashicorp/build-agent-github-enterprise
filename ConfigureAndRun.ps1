b84eb7117fb6b98d9add22b43f9aa64ac0dae257
Jared Holgate <Jared.Holgate@maples.com>
2021-05-19 15:42:59 +0100

    Update ConfigureAndRun.ps1

--- ConfigureAndRun.ps1
+++ ConfigureAndRun.ps1
@@ -1,5 +1,16 @@
+param($platformm = "windows")
+
 $token = $env:GH_RUNNER_TOKEN
 $organisation = $env:GH_RUNNER_ORG
 $gitHubUrl = 'https://github.com/{0}' -f $organisation
-./config.cmd --unattended --url $gitHubUrl --token $token
-./run.cmd
+
+if($platform -eq "windows")
+{
+  ./config.cmd --unattended --url $gitHubUrl --token $token
+  ./run.cmd
+}
+elase
+{
+  ./config.sh --unattended --url $gitHubUrl --token $token
+  ./run.sh
+}
