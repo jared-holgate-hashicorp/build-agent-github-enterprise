a030755a4878ae907ef78f2a7c13591f6e458d66
Jared Holgate <Jared.Holgate@maples.com>
2021-07-05 22:06:29 +0100

    Update ConfigureAndRun.ps1

--- ConfigureAndRun.ps1
+++ ConfigureAndRun.ps1
@@ -3,17 +3,18 @@ param($platform = "windows")
 $token = $env:GH_RUNNER_TOKEN
 $organisation = $env:GH_RUNNER_ORG
 $runnerName = $env:GH_RUNNER_NAME
+$runnerGroup = $env:GH_RUNNER_GROUP
 
 $gitHubUrl = 'https://github.com/{0}' -f $organisation
 
 if($platform -eq "windows")
 {
-  ./config.cmd --unattended --url $gitHubUrl --token $token --name $runnerName
+  ./config.cmd --unattended --url $gitHubUrl --token $token --name $runnerName --runnergroup $runnerGroup
   ./run.cmd
 }
 else
 {
   $env:RUNNER_ALLOW_RUNASROOT = "1"
-  ./config.sh --unattended --url $gitHubUrl --token $token --name $runnerName
+  ./config.sh --unattended --url $gitHubUrl --token $token --name $runnerName --runnergroup $runnerGroup
   ./run.sh
 }
